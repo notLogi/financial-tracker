1463dbb1f6740b757dafcb31f72b63e207141633
notLogi <92322066+notLogi@users.noreply.github.com>
2025-10-10 10:57:43 -0400

    Finished the transaction class

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -1,4 +1,54 @@
 package com.pluralsight;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Transaction {
+    private LocalDate date;
+    private LocalDateTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public LocalDate getDate(){
+        return date;
+    }
+    public void setDate(LocalDate date){
+        this.date = date;
+    }
+    public LocalDateTime getTime(){
+        return time;
+    }
+    public void setTime(LocalDateTime time){
+        this.time = time;
+    }
+    public String getDescription(){
+        return description;
+    }
+    public void setDescription(String description){
+        this.description = description;
+    }
+    public String getVendor(){
+        return vendor;
+    }
+    public void setVendor(String vendor){
+        this.vendor = vendor;
+    }
+    public double getAmount(){
+        return amount;
+    }
+    public void setAmount(double amount){
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "date=" + date +
+                ", time=" + time +
+                ", description='" + description + '\'' +
+                ", vendor='" + vendor + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
